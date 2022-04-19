ca5cd593490ddbfcef77de0c96af3eb9fae11b54
Aleksey Ishutin <ishutin.alexey@gmail.com>
2022-04-19 18:16:56 +0200

    queue support

--- config/server.php
+++ config/server.php
@@ -10,5 +10,6 @@ return [
     'port' => env('SERVER_PORT', 9501),
     'options' => [ // OpenSwoole options
         'worker_num' => env('SERVER_OPTION_WORKER_NUM', 4),
+        'task_worker_num' => env('SERVER_OPTION_TASK_WORKER_NUM', 4), // for Queue
     ],
 ];
